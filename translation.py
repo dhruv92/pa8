c91bbb807fba4f3ee6eb145f8deeb4c92eb813fe
cnegiar <cnegiar@stanford.edu>
2013-03-03 17:57:02 -0800

    added two more rules

--- translation.py
+++ translation.py
@@ -23,6 +23,7 @@ def apply_rules(english):
 
     english_new=''
 
+    # Swap consecutive nouns and adjectives since French and English have a different ordering for these 
     for key, val in pos.items():
         if "NN" in key[1] and val[1]=='JJ':
           swap(words, key[0],  val[0])
@@ -34,19 +35,48 @@ def apply_rules(english):
 
     return english_new
 
+def apply_rules_french(text, my_translations, adjectives):
+    english_text=''
+    for x in range (0, len(text)):
+        word = text[x]
+        if word in my_translations.keys():
+            translation= my_translations[word]
 
-def main():
+            # Since otherwise we get double negation in English. We need to account for the fact that 'ne pas' is actually
+            #one negation, and not two separate negations. 
+            if not word =='pas':
 
-    my_translations=collections.defaultdict(lambda:'My default')
-    text=[]
+                if x < len(text)-1:
+
+                    next_word= text[x+1]
+
+                    exclude = set(string.punctuation)
+                    next_word = ''.join(ch for ch in next_word if ch not in exclude)
+                    
+                    #make the difference between conditional si ('if') and comparative si ('so').
+                    # determines whether preposition 'si' is being used as
+                    #conditional or as comparative ( difference between translating to 'if' or to 'so').
+                    # If followed by an adjective, it is the comparative 'so', and if not, it is the conditional 'if'
+                    if word == 'si' and next_word in adjectives:
+                        translation='so'
+
+                english_text+=translation + ' '
+    return english_text
+
+
+def read_files(my_translations,text,adjectives):
+    for line in open ('adjectives.txt', 'r'):
+        line=line.lower()
+        line= line.replace ('\n', '')
+        adjectives.append(line)
+
     for line in open('liaisons.txt', 'r'):
             line=line.lower()
             line=line.split(' ')
        
             for word in line:
                 text.append(word)
-                
-   
+
     for line in open('dictionary.txt', 'r'):
         line=line.lower()
         line = line.split('-')
@@ -55,20 +85,20 @@ def main():
         english=line[1]
         my_translations[french]=english
 
+def main():
+
+    my_translations=collections.defaultdict(lambda:'My default')
+    text=[]
+    adjectives=[]
+
+    
+    read_files(my_translations,text, adjectives)
+
     english_text=''
 
-    for word in text:
-        if word in my_translations.keys():
-            translation= my_translations[word]
-            if not word.startswith(' '):
-                english_text+= ' ' + translation
-            else : 
-                english_text+=translation
-
+    english_text= apply_rules_french(text,my_translations, adjectives)
     english_text= apply_rules(english_text)
-    print english_text
-
-   
+  
 
 
 if __name__ == '__main__':
